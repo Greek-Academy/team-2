429b738d53ee03e328911264b7b196574c81081b
HonjyouDaisuke <honjyou.daisuke@gmail.com>
2023-09-04 22:02:14 +0900

    ユニットテストを追加しています。

--- src/components/Spinner/Spinner.test.tsx
+++ src/components/Spinner/Spinner.test.tsx
@@ -1,9 +1,12 @@
-import { render, screen } from '@testing-library/react';
+//screenは使われていないので削除
+//import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
 import {Spinner} from './Spinner';
 import '@testing-library/jest-dom/extend-expect'
 
 describe('Spinner', () => {
   it('should render', () => {
-    render(<Spinner />);
+    render(<Spinner visible={true} />);
   });
 });
