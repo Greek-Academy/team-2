456ae0c9d6e75f4b1a90769f26cd1105c7e90d86
HonjyouDaisuke <honjyou.daisuke@gmail.com>
2023-09-05 21:53:39 +0900

    Pull Requestのレビュー結果に対応しました。

--- src/components/MicIcon/MicIcon.test.tsx
+++ src/components/MicIcon/MicIcon.test.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
 import {MicIcon} from './MicIcon';
 import '@testing-library/jest-dom/extend-expect'
-
 describe('MicIcon', () => {
-  it('[Test2] should render', () => {
-    render(<MicIcon visible={true} />);
+  it('MicIcon visible TRUE case', () => {
+    const { getByTestId } = render(<MicIcon visible={true} />)
+    expect(getByTestId("backdrop")).toHaveStyle("visibility: visible;")
+  });
+
+  it('MicIcon visible FALSE case', () => {
+    const { getByTestId } = render(<MicIcon visible={false} />)
+    expect(getByTestId("backdrop")).toHaveStyle("visibility: hidden;")
   });
 });
--- src/components/Spinner/Spinner.test.tsx
+++ src/components/Spinner/Spinner.test.tsx
@@ -1,12 +1,16 @@
-//screenは使われていないので削除
-//import { render, screen } from '@testing-library/react';
 import { render } from '@testing-library/react';
 
 import {Spinner} from './Spinner';
 import '@testing-library/jest-dom/extend-expect'
 
 describe('Spinner', () => {
-  it('should render', () => {
-    render(<Spinner visible={true} />);
+  it('Spinner visible TRUE case', () => {
+    const { getByTestId } = render(<Spinner visible={true} />)
+    expect(getByTestId("backdrop")).toHaveStyle("visibility: visible")
+  });
+
+  it('Spinner visible FALSE case', () => {
+    const { getByTestId } = render(<Spinner visible={false} />)
+    expect(getByTestId("backdrop")).toHaveStyle("visibility: hidden")
   });
 });
